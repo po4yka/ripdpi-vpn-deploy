d002f20845e756e91a54dc8d0e41e9c41ac2c200
Nikita Pochaev <pochaev.nik@gmail.com>
2026-08-28 18:45:28 +0400

    test(deploy): explain process-group cleanup races

--- tests/unit/test_render_inventory.py
+++ tests/unit/test_render_inventory.py
@@ -416,6 +416,7 @@ def test_wait_bounds_connected_ssh_and_kills_its_descendants(tmp_path):
             try:
                 os.killpg(group, signal.SIGKILL)
             except ProcessLookupError:
+                # Deadline/cancellation cleanup may already have removed this group.
                 pass
         process.communicate()
 
@@ -453,6 +454,7 @@ def test_wait_interruption_kills_only_its_ssh_process_group(tmp_path, interrupt)
             try:
                 os.killpg(group, signal.SIGKILL)
             except ProcessLookupError:
+                # Deadline/cancellation cleanup may already have removed this group.
                 pass
         process.communicate()
 
@@ -502,6 +504,7 @@ exec(compile(source, "<production-wait-controller>", "exec"))
             try:
                 os.killpg(group, signal.SIGKILL)
             except ProcessLookupError:
+                # Deadline/cancellation cleanup may already have removed this group.
                 pass
         process.communicate()
 
